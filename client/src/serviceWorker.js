5079c67d06c02434e6b8153a1f2efa60d3f35ab5
hhundley <65366145+hhundley@users.noreply.github.com>
2022-12-07 16:23:17 -0600

    service worker

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -35,7 +35,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = './service-worker.js';
+      const swUrl = './serviceWorker.js';
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
