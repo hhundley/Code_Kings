cdecc3a3ddd91e71cdb2f6e8ad3c605edd5fe68b
hhundley <65366145+hhundley@users.noreply.github.com>
2022-12-06 21:10:17 -0600

    deploy

--- server/server.js
+++ server/server.js
@@ -24,9 +24,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
