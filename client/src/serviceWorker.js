7058dfe504f5541964734214529737d60fab4b75
hhundley <65366145+hhundley@users.noreply.github.com>
2022-12-07 16:28:09 -0600

    another attempt at fixing service worker

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -35,7 +35,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = './serviceWorker.js';
+      const swUrl = '%PUBLIC_URL%/serviceWorker.js';
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
